2327a66c9830ec673db69c635b1e004b9c43850d
Amir Reza Mehrbakhsh <amirreza@dakyaco.com>
2020-06-25 23:11:33 +0430

    16 - Webpack and Vue CLI

---
+++ 16 - Webpack and Vue CLI/my-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <message title="Hello World">This is content.</message>
+  </div>
+</template>
+
+<script>
+  import Message from './components/Message.vue';
+
+  export default {
+    components: { Message },
+    name: 'app',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
